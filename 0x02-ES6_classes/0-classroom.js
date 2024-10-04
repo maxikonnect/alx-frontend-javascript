7670851e4c4fd8b2059abc8b9923b31d1f3cf39b
maxikonnect <tribillio@gmail.com>
2024-10-04 04:40:19 +0000

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  }
